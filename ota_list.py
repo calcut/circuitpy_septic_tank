038d5926fa9ebd74dc352affe04e3ca57df7c1f4
calcut <58563665+calcut@users.noreply.github.com>
2022-05-30 09:56:11 +0100

    Update ota_list.py

--- ota_list.py
+++ ota_list.py
@@ -1,4 +1,4 @@
 {
-  "/ota/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/mcu.py"
-  "/ota/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/septic_tank.py"
+  "/sd/ota/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/mcu.py"
+  "/sd/ota/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/septic_tank.py"
 }
