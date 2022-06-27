4dba100bd0efe6c2da703800641ad51da88a6028
Calum Cuthill <calum.cuthill@gmail.com>
2022-06-27 17:17:02 +0100

    simplified, fails still occasionally, about to try CP8

--- septic_tank_aio_debug.py
+++ septic_tank_aio_debug.py
@@ -1,84 +1,62 @@
 import time
-from circuitpy_mcu.mcu import Mcu
-
-# scheduling and event/error handling libs
-from watchdog import WatchDogTimeout
 import supervisor
-import microcontroller
-import adafruit_logging as logging
-import traceback
+import wifi, ssl, socketpool
+import adafruit_minimqtt.adafruit_minimqtt as MQTT
+from adafruit_io.adafruit_io import IO_MQTT
+import adafruit_requests
 
-__version__ = "0.0.0-auto.0"
-__repo__ = "https://github.com/calcut/circuitpy-septic_tank"
-__filename__ = "septic_tank.py"
-
-# Set AIO = True to use Wifi and Adafruit IO connection
-# secrets.py file needs to be setup appropriately
-AIO = True
+from secrets import secrets
 
 AIO_GROUP = 'septic-dev'
-LOGLEVEL = logging.INFO
+
+def aio_subscribe_callback(client, userdata, topic, granted_qos):
+    print(f"Subscribed to {topic} with QOS level {granted_qos}")
 
 
+ssid = secrets["ssid"]
+password = secrets["password"]
+wifi.radio.connect(ssid, password)
 
-def main():
+pool = socketpool.SocketPool(wifi.radio)
+requests = adafruit_requests.Session(pool, ssl.create_default_context())
 
-    # instantiate the MCU helper class to set up the system
-    mcu = Mcu(watchdog_timeout=20)
+mqtt_client = MQTT.MQTT(
+    broker="io.adafruit.com",
+    username=secrets["aio_username"],
+    password=secrets["aio_key"],
+    socket_pool=pool,
+    ssl_context=ssl.create_default_context(),
+)
 
-    # Choose minimum logging level to process
-    mcu.log.setLevel(LOGLEVEL)
+io = IO_MQTT(mqtt_client)
+io.on_subscribe = aio_subscribe_callback
+io.connect()
 
- 
-    mcu.watchdog.feed()
+# io.subscribe('TC1')
+# io.subscribe('TC2')
+# io.subscribe('TC3')
+# io.subscribe('TC4')
+# io.subscribe('TC5')
+# io.subscribe('TC6')
+# io.subscribe('TC7')
+# io.subscribe('TC8')
 
-    if AIO:
-        mcu.wifi_connect()
-        mcu.aio_setup(log_feed='log', group=AIO_GROUP)
+
+io.subscribe(f'{AIO_GROUP}.pump1-speed')
+io.subscribe(f'{AIO_GROUP}.pump2-speed')
+io.subscribe(f'{AIO_GROUP}.pump3-speed')
+io.subscribe(f'{AIO_GROUP}.gc1')
+io.subscribe(f'{AIO_GROUP}.gc2')
+io.subscribe(f'{AIO_GROUP}.gc3')
+io.subscribe(f'{AIO_GROUP}.tc1')
+io.subscribe(f'{AIO_GROUP}.tc2')
+io.subscribe(f'{AIO_GROUP}.tc3')
+io.subscribe(f'{AIO_GROUP}.tc4')
+io.subscribe(f'{AIO_GROUP}.ph1')
+io.subscribe(f'{AIO_GROUP}.ph2')
+io.subscribe(f'{AIO_GROUP}.ph3')
 
 
-        
-        mcu.subscribe(f'{AIO_GROUP}.pump1-speed')
-        mcu.subscribe(f'{AIO_GROUP}.pump2-speed')
-        mcu.subscribe(f'{AIO_GROUP}.pump3-speed')
-        mcu.subscribe(f'{AIO_GROUP}.gc1')
-        mcu.subscribe(f'{AIO_GROUP}.gc2')
-        mcu.subscribe(f'{AIO_GROUP}.gc3')
-        mcu.subscribe(f'{AIO_GROUP}.tc1')
-        mcu.subscribe(f'{AIO_GROUP}.tc2')
-        mcu.subscribe(f'{AIO_GROUP}.tc3')
-        mcu.subscribe(f'{AIO_GROUP}.tc4')
-        mcu.subscribe(f'{AIO_GROUP}.ph1')
-        mcu.subscribe(f'{AIO_GROUP}.ph2')
-        mcu.subscribe(f'{AIO_GROUP}.ph3')
-
- 
-    mcu.log.info(f'BOOT complete at {mcu.get_timestamp()}')
-    time.sleep(2)
-    supervisor.reload()
-
- 
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Code Stopped by Keyboard Interrupt')
-        # May want to add code to stop gracefully here 
-        # e.g. turn off relays or pumps
-        
-    except WatchDogTimeout:
-        print('Code Stopped by WatchDog Timeout!')
-        # supervisor.reload()
-        # NB, sometimes soft reset is not enough! need to do hard reset here
-        print('Performing hard reset in 15s')
-        # time.sleep(15)
-        # microcontroller.reset()
-
-    except Exception as e:
-        print(f'Code stopped by unhandled exception:')
-        print(traceback.format_exception(None, e, e.__traceback__))
-        # Can we log here?
-        print('Performing a hard reset in 15s')
-        # time.sleep(15) #Make sure this is shorter than watchdog timeout
-        # supervisor.reload()
-        # microcontroller.reset()
+print(f'BOOT complete')
+time.sleep(2)
+supervisor.reload()
