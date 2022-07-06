8f339e913bf636e53198c13ec5cb5c88280398be
Calum Cuthill <calum.cuthill@gmail.com>
2022-07-06 13:07:33 +0100

    trying to have reset and watchdog handled here

--- code.py
+++ code.py
@@ -1,11 +1,28 @@
+
+from watchdog import WatchDogTimeout
 import supervisor
+import traceback
+
 supervisor.disable_autoreload()
 
 # If CIRCUITPY drive is writable (configured in boot.py) this will update code files over-the-air
-from circuitpy_mcu.ota_bootloader import Bootloader
+from circuitpy_mcu.ota_bootloader import Bootloader, reset, enable_watchdog
 url = 'https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/ota_list.py'
+
+enable_watchdog(timeout=120)
 bl = Bootloader(url)
 
-code = '/circuitpy_septic_tank/septic_tank.py'
-supervisor.set_next_code_file(code, reload_on_success=False)
-supervisor.reload()
+from circuitpy_septic_tank.septic_tank import main
+# from circuitpy_mcu.adafruit_io_http_test import main
+
+try:
+    main()
+except KeyboardInterrupt:
+    print('Code Stopped by Keyboard Interrupt')
+    # May want to add code to stop gracefully here 
+    # e.g. turn off relays or pumps
+
+except Exception as e:
+    print(f'Code stopped by unhandled exception:')
+    detail = traceback.format_exception(None, e, e.__traceback__)
+    reset(detail)
