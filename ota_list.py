acbdeb7415f4e44806785cd57428c774099482d3
calcut <58563665+calcut@users.noreply.github.com>
2022-07-07 11:28:43 +0100

    added aio.py

--- ota_list.py
+++ ota_list.py
@@ -1,5 +1,6 @@
 {
   "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/mcu.py",
+  "/circuitpy_mcu/aio.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/aio.py",
   "/circuitpy_mcu/display.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/display.py",
   "/circuitpy_mcu/DFRobot_PH.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/DFRobot_PH.py",
   "/circuitpy_septic_tank/gascard.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/gascard.py",
