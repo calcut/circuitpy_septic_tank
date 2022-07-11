8605af69d7cff77b45b0c5d0d1c7991f553dab3c
calcut <58563665+calcut@users.noreply.github.com>
2022-07-11 11:25:36 +0100

    trying different mcu path. in case of corruption

--- ota_list.py
+++ ota_list.py
@@ -1,5 +1,5 @@
 {
-    "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v1.0.0-rc2/mcu.py",
+    "/circuitpy_mcu/mcu_debug.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v1.0.0-rc2/mcu.py",
     "/circuitpy_mcu/aio.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v1.0.0-rc2/aio.py",
     "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v1.0.0-rc1/septic_tank.py"
 }
