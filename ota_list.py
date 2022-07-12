9bfbd5ef776da0fac4e288c8c56467c79f30a611
calcut <58563665+calcut@users.noreply.github.com>
2022-07-12 20:57:42 +0100

    Update ota_list.py

--- ota_list.py
+++ ota_list.py
@@ -12,7 +12,7 @@
       "/circuitpy_mcu/aio.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v1.0.0-rc2/aio.py",
       "/circuitpy_mcu/display.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v1.0.0-rc2/display.py",
       "/circuitpy_mcu/DFRobot_PH.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v1.0.0-rc2/DFRobot_PH.py",
-      "/circuitpy_septic_tank/gascard.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v1.0.0-rc2/gascard.py",
-      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v1.0.0-rc2/septic_tank.py"
+      "/circuitpy_septic_tank/gascard.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v1.0.0-rc1/gascard.py",
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v1.0.0-rc1/septic_tank.py"
     }
 }
