aa5c4fd17fc453a0682e6c9ceedc42da389df81f
calcut <58563665+calcut@users.noreply.github.com>
2022-07-21 16:07:37 +0100

    more gascard interval changes

--- septic_tank.py
+++ septic_tank.py
@@ -24,8 +24,8 @@ __filename__ = "septic_tank.py"
 AIO = True
 # AIO = False
 
-GASCARD_PUMP_TIME = 300 #5 minutes
-GASCARD_INTERVAL = 420 #7 minutes
+GASCARD_PUMP_TIME = 120 #2 minutes
+GASCARD_INTERVAL = 1200 #20 minutes
 GASCARD = True
 # GASCARD = False
 NUM_PUMPS = 2
