263c9e90f7e7ab0ec25a586d4a7522bf1ebb116c
Calum <58563665+calcut@users.noreply.github.com>
2022-11-01 13:45:12 +0000

    Update ota_list.py

--- ota_list.py
+++ ota_list.py
@@ -7,15 +7,13 @@
   },
   "f247" : {
       "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v2-dev-offline/mcu.py",
-      "/lib/adafruit_sdcard.py" : "https://raw.githubusercontent.com/calcut/Adafruit_CircuitPython_SD/main/adafruit_sdcard.py"
+      "/lib/adafruit_sdcard.py" : "https://raw.githubusercontent.com/calcut/Adafruit_CircuitPython_SD/main/adafruit_sdcard.py",
       "/circuitpy_septic_tank/solenoid.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/mcu-v2-testing/solenoid.py"
   },
   "1205" : {
-      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v2-dev-offline/mcu.py",
-      "/circuitpy_mcu/aio.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v2-dev-offline/aio.py",
-      "/circuitpy_mcu/display.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v2-dev-offline/display.py",
-      "/circuitpy_mcu/DFRobot_PH.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v2-dev-offline/DFRobot_PH.py",
-      "/circuitpy_septic_tank/gascard.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/mcu-v2-testing/gascard.py",
-      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/mcu-v2-testing/septic_tank.py"
-    }
+      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v2.0.0/mcu.py",
+      "/circuitpy_mcu/aio.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v2.0.0/aio.py",
+      "/circuitpy_septic_tank/gascard.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v2.0.0/gascard.py",
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v2.0.0/septic_tank.py"
+  }
 }
