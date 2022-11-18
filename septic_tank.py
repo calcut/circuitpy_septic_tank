1417092f1a5a25ef889d816dfe42ef80008e5f8a
Calum Cuthill <calum.cuthill@gmail.com>
2022-11-18 16:50:24 +0000

    update version

--- septic_tank.py
+++ septic_tank.py
@@ -15,7 +15,7 @@ import board
 import adafruit_logging as logging
 
 
-__version__ = "3.0.0_notecard"
+__version__ = "3.0.1_notecard"
 __repo__ = "https://github.com/calcut/circuitpy-septic_tank"
 __filename__ = "septic_tank.py"
 
