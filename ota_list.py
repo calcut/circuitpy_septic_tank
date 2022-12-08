de4dbcce558213f016599fdec959e582a217fdd2
Calum <58563665+calcut@users.noreply.github.com>
2022-12-08 14:49:01 +0000

    Update ota_list.py

--- ota_list.py
+++ ota_list.py
@@ -2,7 +2,7 @@
   "f627" : {
       "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.0/mcu.py",
       "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.0/notecard_manager.py",
-      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/notecard/septic_tank.py"
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.1.0/septic_tank.py"
   },
   "f247" : {
       "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v2-dev-offline/mcu.py",
@@ -12,6 +12,6 @@
   "1205" : {
       "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.0/mcu.py",
       "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.0/notecard_manager.py",
-      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/notecard/septic_tank.py"
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.1.0/septic_tank.py"
   }
 }
