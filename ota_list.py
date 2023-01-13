51ceb396a4dd96f216a020d1f2da17c21877457a
Calum <58563665+calcut@users.noreply.github.com>
2023-01-13 13:40:33 +0000

    Update ota_list.py

--- ota_list.py
+++ ota_list.py
@@ -4,14 +4,24 @@
       "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/notecard_manager.py",
       "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/septic_tank.py"
   },
-  "f247" : {
-      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/mcu.py",
-      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/notecard_manager.py",
-      "/circuitpy_septic_tank/solenoid.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.1.1/feed_control.py"
-  },
   "1205" : {
       "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/mcu.py",
       "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/notecard_manager.py",
       "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/septic_tank.py"
+  },
+  "856d" : {
+      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/mcu.py",
+      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/notecard_manager.py",
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/septic_tank.py"
+  },
+  "81aa" : {
+      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/mcu.py",
+      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/notecard_manager.py",
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/septic_tank.py"
+  },
+  "f247" : {
+      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/mcu.py",
+      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/notecard_manager.py",
+      "/circuitpy_septic_tank/solenoid.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.1.1/feed_control.py"
   }
 }
