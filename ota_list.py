fff522bd8e92f0b0f54b3080f307f6b2006d4aa6
Calum <58563665+calcut@users.noreply.github.com>
2023-01-20 10:50:51 +0000

    Update ota_list.py

--- ota_list.py
+++ ota_list.py
@@ -22,6 +22,6 @@
   "f247" : {
       "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/mcu.py",
       "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/notecard_manager.py",
-      "/circuitpy_septic_tank/solenoid.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.1.1/feed_control.py"
+      "/circuitpy_septic_tank/solenoid.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/feed_control.py"
   }
 }
