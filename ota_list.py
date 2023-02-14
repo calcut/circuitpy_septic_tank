bfe1258add5651611107fbc982210a47bb0cb9e1
Calum Cuthill <calum.cuthill@gmail.com>
2023-02-14 16:27:07 +0000

    version and tag update for valve, pressure measurement

--- ota_list.py
+++ ota_list.py
@@ -1,27 +1,27 @@
 {
   "f627" : {
-      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/mcu.py",
-      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/notecard_manager.py",
-      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/septic_tank.py"
+      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.3/mcu.py",
+      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.3/notecard_manager.py",
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.2.0/septic_tank.py"
   },
   "1205" : {
-      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/mcu.py",
-      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/notecard_manager.py",
-      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/septic_tank.py"
+      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.3/mcu.py",
+      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.3/notecard_manager.py",
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.2.0/septic_tank.py"
   },
   "856d" : {
-      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/mcu.py",
-      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/notecard_manager.py",
-      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/septic_tank.py"
+      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.3/mcu.py",
+      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.3/notecard_manager.py",
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.2.0/septic_tank.py"
   },
   "81aa" : {
-      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/mcu.py",
-      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/notecard_manager.py",
-      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/septic_tank.py"
+      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.3/mcu.py",
+      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.3/notecard_manager.py",
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.2.0/septic_tank.py"
   },
   "f247" : {
-      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.2/mcu.py",
-      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/notecard_manager.py",
-      "/circuitpy_septic_tank/feed_control.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/feed_control.py"
+      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.3/mcu.py",
+      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.3/notecard_manager.py",
+      "/circuitpy_septic_tank/feed_control.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.2.0/feed_control.py"
   }
 }
