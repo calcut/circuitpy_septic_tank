95614f982d1e93ae9579e9aa2ff4fd379a8f01c7
Calum Cuthill <calum.cuthill@gmail.com>
2023-03-12 15:42:04 +0000

    version number update

--- ota_list.py
+++ ota_list.py
@@ -2,22 +2,22 @@
   "f627" : {
       "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.3/mcu.py",
       "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/notecard_manager.py",
-      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.2.0/septic_tank.py"
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.3.0/septic_tank.py"
   },
   "1205" : {
       "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.3/mcu.py",
       "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/notecard_manager.py",
-      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.2.0/septic_tank.py"
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.3.0/septic_tank.py"
   },
   "856d" : {
       "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.3/mcu.py",
       "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/notecard_manager.py",
-      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.2.0/septic_tank.py"
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.3.0/septic_tank.py"
   },
   "81aa" : {
       "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.3/mcu.py",
       "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/notecard_manager.py",
-      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.2.0/septic_tank.py"
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.3.0/septic_tank.py"
   },
   "f247" : {
       "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.3/mcu.py",
