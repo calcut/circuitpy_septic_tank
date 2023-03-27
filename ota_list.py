48c4516ba99880c6c80c2b1d40c541b9b449cda4
Calum <58563665+calcut@users.noreply.github.com>
2023-03-27 10:44:18 +0100

    Update ota_list.py
    added TEST system

--- ota_list.py
+++ ota_list.py
@@ -19,6 +19,11 @@
       "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.4/notecard_manager.py",
       "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.3.1/septic_tank.py"
   },
+  "2b41" : {
+      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.4/mcu.py",
+      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.4/notecard_manager.py",
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.3.1/septic_tank.py"
+  },
   "f247" : {
       "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.4/mcu.py",
       "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.4/notecard_manager.py",
