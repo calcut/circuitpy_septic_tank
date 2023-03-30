3fcfb4b18db90aea1c87748a95596418fcabec99
Calum <58563665+calcut@users.noreply.github.com>
2023-03-30 18:19:28 +0100

    Update ota_list.py
    feedcontrol notecard manger on main to switch to localtime instead of UTC

--- ota_list.py
+++ ota_list.py
@@ -25,8 +25,8 @@
       "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/septic_tank.py"
   },
   "f247" : {
-      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.4/mcu.py",
-      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/v3.1.4/notecard_manager.py",
+      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/mcu.py",
+      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/notecard_manager.py",
       "/circuitpy_septic_tank/feed_control.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.3.2/feed_control.py",
       "/circuitpy_septic_tank/solenoid_valve.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/v3.3.2/solenoid_valve.py"
   }
