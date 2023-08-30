e5d67dfec7b62320b98ca8cf4cceeb37fe7b4915
Calum <58563665+calcut@users.noreply.github.com>
2023-08-30 12:13:31 +0100

    Update ota_list.py
    mcu on manual switches branch to disable led

--- ota_list.py
+++ ota_list.py
@@ -25,7 +25,7 @@
       "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/septic_tank.py"
   },
   "f247" : {
-      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/mcu.py",
+      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/manual_switches/mcu.py",
       "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/notecard_manager.py",
       "/circuitpy_septic_tank/feed_control.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/manual_switches/feed_control.py",
       "/circuitpy_septic_tank/solenoid_valve.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/solenoid_valve.py"
