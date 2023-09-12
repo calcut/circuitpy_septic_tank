28ce7a911609ed3fae5fe956fcbca5b7586c2d61
Calum <58563665+calcut@users.noreply.github.com>
2023-09-12 12:55:42 +0100

    Update ota_list.py

--- ota_list.py
+++ ota_list.py
@@ -12,7 +12,7 @@
   "856d" : {
       "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/mcu.py",
       "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/notecard_manager.py",
-      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/minimal_boot/septic_tank.py"
+      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/septic_tank.py"
   },
   "81aa" : {
       "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/mcu.py",
