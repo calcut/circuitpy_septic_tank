cdc85fc9b00c93f6e55c253255761dba79cadb33
Calum <58563665+calcut@users.noreply.github.com>
2023-11-07 11:21:48 +0000

    replaced feedcontrol feather

--- ota_list.py
+++ ota_list.py
@@ -21,11 +21,6 @@
       "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/septic_tank.py"
   },
   "2b41" : {
-      "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/mcu.py",
-      "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/notecard_manager.py",
-      "/circuitpy_septic_tank/septic_tank.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/main/septic_tank.py"
-  },
-  "f247" : {
       "/circuitpy_mcu/mcu.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/manual_switches/mcu.py",
       "/circuitpy_mcu/notecard_manager.py" : "https://raw.githubusercontent.com/calcut/circuitpy_mcu/main/notecard_manager.py",
       "/circuitpy_septic_tank/feed_control.py" : "https://raw.githubusercontent.com/calcut/circuitpy_septic_tank/manual_switches/feed_control.py",
